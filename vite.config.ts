f9f05c038ce3ad225f2dddd649bde7926f00e0e6
James Gopsill <james.gopsill@cfms.org.uk>
2022-11-03 15:03:41 +0000

    Adding baseUrl config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { svelte } from "@sveltejs/vite-plugin-svelte"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "./",
 	plugins: [svelte()],
 	build: {
 		outDir: "docs",
-		emptyOutDir: true
-	}
+		emptyOutDir: true,
+	},
 })
