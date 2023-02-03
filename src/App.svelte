afaf6da0bb6b92ffce0a7b4491c9f8e434cd72dd
James Gopsill <james.gopsill@gmail.com>
2023-02-03 10:14:20 +0000

    start refactoring

--- src/App.svelte
+++ src/App.svelte
@@ -1,53 +1,13 @@
 <script lang="ts">
 	import {
+		Container,
 		Navbar,
 		NavbarBrand,
-		Container,
 		TabContent,
 		TabPane,
 	} from "sveltestrap"
-	import Job from "./Job.svelte"
-	import Machine from "./Machine.svelte"
-	import Settings from "./Settings.svelte"
-	//@ts-ignore
-	import logoUrl from "./assets/logo.png"
-	//@ts-ignore
-	import epsrcUrl from "./assets/epsrc.png"
-	import Tracker from "./Tracker.svelte"
 </script>
 
 <Navbar color="light" light={true} expand="md" class="mb-1">
 	<NavbarBrand href="#">BAM Living Lab</NavbarBrand>
 </Navbar>
-
-<Container xxl>
-	<p class="text-center">
-			<img
-				alt=""
-				height="60"
-				src={logoUrl}
-			/>
-			&nbsp;
-			<img
-				alt=""
-				height="60"
-				class="mt-2"
-				src={epsrcUrl}
-			/>
-		</p>
-	<TabContent>
-		<TabPane class="mt-1" tabId="job" tab="Submit Job" active>
-			<Job />
-		</TabPane>
-		<TabPane class="mt-1" tabId="machine" tab="Connect a Machine">
-			<Machine />
-		</TabPane>
-		<TabPane class="mt-1" tabId="settings" tab="Settings">
-			<Settings />
-		</TabPane>
-		<TabPane class="mt-1" tabId="tracking" tab="Tracking">
-			<Tracker />
-		</TabPane>
-	</TabContent>
-	<footer class="text-center"><hr/><p>👨‍💻 with 🧡 and ☕</p></footer>
-</Container>
